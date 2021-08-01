dfee188ad3b2e4510b2e53f7799a3ba6a9b4c668
Tupi Guedes <tupiguedes@outlook.com>
2021-08-01 19:12:27 -0300

    Projeto iniciado

---
+++ src/entities/Produto.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Produto {
+
+	private String nome;
+	private Double preco;
+	private Integer quantidade;
+
+	public Produto(String nome, Double preco, Integer quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+}
