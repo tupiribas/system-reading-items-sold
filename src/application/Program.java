20b4af8ee7adbd7e957d81df8a207846c1523a4c
Tupi Guedes <tupiguedes@outlook.com>
2021-08-01 19:47:27 -0300

    printing the file information without the comma

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,35 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Digite as informações do produto: ");
-		String info = sc.nextLine();
+		Scanner sc = null;
+		File file = new File("C:\\pastaarquivo\\in.txt");
 		
-		String i[] = new String[3];
-		i = info.split(",");
-		
-		for (String s : i) {
-			System.out.print(s);
+		try {
+			String i[] = new String[3];
+			
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				i = sc.nextLine().split(",");
+			}
+			
+			System.out.println("Informações: ");
+			for (String s : i) {
+				System.out.print(s);
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Erro >>> " + e.getMessage());
+		}
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
 		}
 		
 		
