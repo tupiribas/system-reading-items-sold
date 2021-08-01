46dc8c480d5177b68553357b57c646cfc6c8a27b
Tupi Guedes <tupiguedes@outlook.com>
2021-08-01 19:31:38 -0300

    Separating the comma from the information

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,15 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Digite as informações do produto: ");
+		String info = sc.nextLine();
+		
+		String i[] = new String[3];
+		i = info.split(",");
+		
+		for (String s : i) {
+			System.out.print(s);
+		}
 		
 		
 		sc.close();
